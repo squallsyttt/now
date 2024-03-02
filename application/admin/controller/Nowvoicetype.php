37aba564af81f8e136b3d1bad9c529cf2364af21
Griffith <390239178@qq.com>
2024-03-02 21:21:10 +0800

    声音管理 声音类型 selectpage修改

--- application/admin/controller/Nowvoicetype.php
+++ application/admin/controller/Nowvoicetype.php
@@ -3,6 +3,7 @@
 namespace app\admin\controller;
 
 use app\common\controller\Backend;
+use think\Db;
 
 /**
  * 
@@ -25,13 +26,22 @@ class Nowvoicetype extends Backend
 
     }
 
-
-
     /**
      * 默认生成的控制器所继承的父类中有index/add/edit/del/multi五个基础方法、destroy/restore/recyclebin三个回收站方法
      * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
 
-
+    /**
+     * 声音管理时 关联获取声音类型data-source 的数据
+     * @return array
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function getVoiceTypeSelect()
+    {
+        $typeList = Db::name('nowvoicetype')->field('type_id,type_name')->select();
+        return ['list' => $typeList,'total' => count($typeList)];
+    }
 }
