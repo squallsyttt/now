3c83395f9a7c675cd8d86c84b0d976ec56cb9c73
Griffith <390239178@qq.com>
2024-03-03 11:58:37 +0800

    呼吸设置全部完成

--- application/extra/site.php
+++ application/extra/site.php
@@ -15,24 +15,18 @@ return array (
   'fixedpage' => 'dashboard',
   'categorytype' => 
   array (
-    'default' => 'Default',
-    'page' => 'Page',
-    'article' => 'Article',
+    'default' => '默认',
+    'page' => '单页',
+    'article' => '文章',
     'test' => 'Test',
   ),
   'configgroup' => 
   array (
-    'basic' => 'Basic',
-    'email' => 'Email',
-    'dictionary' => 'Dictionary',
-    'user' => 'User',
-    'example' => 'Example',
-  ),
-  'attachmentcategory' => 
-  array (
-    'category1' => 'Category1',
-    'category2' => 'Category2',
-    'custom' => 'Custom',
+    'basic' => '基础配置',
+    'email' => '邮件配置',
+    'dictionary' => '字典配置',
+    'user' => '会员配置',
+    'example' => '示例分组',
   ),
   'mail_type' => '1',
   'mail_smtp_host' => 'smtp.qq.com',
@@ -41,4 +35,11 @@ return array (
   'mail_smtp_pass' => 'password',
   'mail_verify_type' => '2',
   'mail_from' => '10000@qq.com',
+  'attachmentcategory' => 
+  array (
+    'category1' => '呼吸分类',
+    'category2' => '呼吸背景音分类',
+    'category3' => '助眠分类',
+    'category4' => '声音分类',
+  ),
 );
